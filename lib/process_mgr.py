c24310f308439076c8507d4ebe4642ef1910f713
prim <1210102@qq.com>
2016-03-06 13:41:00 +0800

    x

--- lib/process_mgr.py
+++ lib/process_mgr.py
@@ -2,55 +2,51 @@
 import log
 import subprocess
 
-class ProcessManager(object):
+_processes = {}
 
-    def __init__(self):
-        self.processes = {}
+def start_process(cmd, **kw):
+    popen = subprocess.Popen(cmd.split(" "), **kw)
+    pid = popen.pid
+    log.info("start process %d %s ", pid, cmd)
+    _processes[pid] = popen
+    return pid
 
-    def start_process(self, cmd, **kw):
-        popen = subprocess.Popen(cmd.split(" "), **kw)
-        pid = popen.pid
-        log.info("start process %d %s ", pid, cmd)
-        self.processes[pid] = popen
-        return pid
+def kill_process(pid):
+    if pid not in _processes:
+        log.error("kill process pid not found %d", pid)
+        return 0
+    log.info("kill process pid %d", pid)
+    popen = _processes[pid]
+    popen.kill()
+    return 1
 
-    def kill_process(self, pid):
-        if pid not in self.processes:
-            log.error("kill process pid not found %d", pid)
-            return 0
-        log.info("kill process pid %d", pid)
-        popen = self.processes[pid]
-        popen.kill()
-        return 1
-
-    def poll(self):
-        for pid, popen in self.processes.items():
-            retstatus = self.processes[pid].poll()
-            if retstatus is not None:
-                log.info("process mgr poll process %d terminated %d", pid, retstatus)
-                del self.processes[pid]
+def poll():
+    for pid, popen in _processes.items():
+        retstatus = _processes[pid].poll()
+        if retstatus is not None:
+            log.info("process mgr poll process %d terminated %d", pid, retstatus)
+            del _processes[pid]
 
 if __name__ == "__main__":
 
-    mgr = ProcessManager()
-    pid = mgr.start_process("ls -l")
-    mgr.kill_process(pid)
+    pid = start_process("ls -l")
+    kill_process(pid)
 
-    pid = mgr.start_process("python")
-    mgr.kill_process(pid)
+    pid = start_process("python")
+    kill_process(pid)
 
-    pid = mgr.start_process("cd", shell=True)
-    mgr.kill_process(pid)
+    pid = start_process("cd", shell=True)
+    kill_process(pid)
 
-    pid = mgr.start_process("dir", shell=True)
-    mgr.kill_process(pid)
+    pid = start_process("dir", shell=True)
+    kill_process(pid)
 
-    pid = mgr.start_process("sleep 5")
-    mgr.poll()
+    pid = start_process("sleep 5")
+    poll()
     import time
     time.sleep(3)
-    mgr.kill_process(pid)
+    kill_process(pid)
 
     time.sleep(3)
-    mgr.poll()
+    poll()
 
